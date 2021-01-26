1f613fd5e8957363485a45113faea9e3d43f6184
alexey.zharkov <alexey.zharkov@travelline.ru>
2021-01-26 20:26:42 +0300

    Подготовка к защите

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@
   const navToggle = document.querySelector('.nav-toggle'),
         pageHeader = document.querySelector('.header');
 
+  pageHeader.classList.remove('header--no-js');
   navToggle.addEventListener('click', () => {
     pageHeader.classList.toggle('header--open-menu');
   })
