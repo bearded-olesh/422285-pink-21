3dcc9619273c185223f3bcb16f1f9e8980ed7b21
alexey.zharkov <alexey.zharkov@travelline.ru>
2021-01-26 04:04:35 +0300

    Подготовка к защите

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const del = require("del");
 
 // Styles
 
@@ -17,18 +20,78 @@ const styles = () => {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css/"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/**/*.js")
+    .pipe(gulp.dest("build/js/"))
+    .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
+
+// Pages
+
+const html = () => {
+  return gulp.src("source/**/*.html")
+    .pipe(gulp.dest("build/"))
+    .pipe(sync.stream());
+}
+
+exports.html = html;
+
+// Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+}
+exports.images = images;
+
+// CreateWebp
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img/webp"))
+}
+exports.createWebp = createWebp;
+
+// Copy
+
+const copy = () => {
+  return gulp.src(["source/fonts/**/*.{woff2,woff}"],
+    {
+      base: "source"
+    })
+    .pipe(gulp.dest("build/"))
+}
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("build")
+}
+exports.clean = clean;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -43,9 +106,27 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/js/**/*.js", gulp.series("scripts"));
+  gulp.watch("source/**/*.html", gulp.series("html"));
+  gulp.watch("source/**/*.html").on("change", sync.reload);
 }
 
+// Build
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(
+    styles,
+    scripts,
+    html,
+    images,
+    createWebp,
+    copy
+  )
+)
+
+exports.build = build;
+
 exports.default = gulp.series(
-  styles, server, watcher
+  styles, scripts, html, server, watcher
 );
